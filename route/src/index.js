2fdf1893e61403d2479455541ddafc60ee220a77
srshin2020 <sangrimshin2020@gmail.com>
2022-01-16 01:41:56 +0900

    react-router-dom

--- route/src/index.js
+++ route/src/index.js
@@ -1,15 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+function Home() {
+  return (
+    <div className="article" >Home....</div>
+  )
+}
+
+function Topics() {
+  return (
+    <div className="article" >Topic...</div>
+  )
+}
+
+function Contact() {
+  return (
+    <div className="article" >Contact...</div>
+  )
+}
+
+function App() {
+  return (
+    <div className="container">
+      <ul className="nav">
+        <li><a href="/">Home</a></li>
+        <li><a href="/topics">Topics</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+      <Routes >
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/topics" element={<Topics />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
+    </div>
+  )
+}
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
