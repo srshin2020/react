8aad891b9f5844b56fb315a9676bc3386666fd2b
srshin2020 <sangrimshin2020@gmail.com>
2022-01-27 23:09:02 +0900

    express server 설치

---
+++ boardServer/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const port = 5000;
+
+app.get ('/', (req, res)=>{
+    res.send("Hello world")
+})
+
+app.listen (port, ()=>{
+    console.log(`Server started... \nListening port on ${port}....`) 
+})
