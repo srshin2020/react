ad01ec06f8403fd2eb3b1dec58505b7254386bc1
srshin2020 <sangrimshin2020@gmail.com>
2022-02-03 22:32:30 +0900

    appbar

--- react-firebase-app/src/component/App.js
+++ react-firebase-app/src/component/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
+import AppShell from './AppShell';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <h3>hello world!</h3>
-            </div>
+            <AppShell></AppShell>
         );
     }
 }
--- react-firebase-app/src/component/AppShell.js
+++ react-firebase-app/src/component/AppShell.js
@@ -1,26 +1,44 @@
 import React from "react";
-import withStyles from '@material-ui/core/styles';
-import { AppBar } from "@material-ui/core";
-import { Drawer } from "@material-ui/core";
-import { MenuItem } from "@material-ui/core";
-import { IconButton } from "@material-ui/core";
-import MenuIcon from '@material-ui/core/Menu';
+import { withStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import MenuItem from "@material-ui/core/MenuItem";
+import AppBar from "@material-ui/core/AppBar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const styles = {
-    root : {
-        flexGrow : 1, 
+    root: {
+        flexGrow: 1,
     },
-    menuButton : {
-        marginRight : auto //왼쪽으로 정렬되는 효과
-    }
-}
+    menuButton: {
+        marginRight: "auto", //왼쪽으로 정렬되는 효과
+    },
+};
 
 class AppShell extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            toggle: false
-        }
+            toggle: false,
+        };
     }
-    handleDrawerToggle = () => this.setState({ toggle: !this.state.toggle })
+    handleDrawerToggle = () => this.setState({ toggle: !this.state.toggle });
+    render() {
+        const { classes } = this.props;
+        return (
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <IconButton className={classes.menuButton} color="inherit" onClick={this.handleDrawerToggle}>
+                        <MenuIcon />
+                    </IconButton>
+                </AppBar>
+                <Drawer open={this.state.toggle}>
+                    <MenuItem onClick={this.handleDrawerToggle}>Home</MenuItem>
+                    <MenuItem onClick={this.handleDrawerToggle}>Home2</MenuItem>
+                </Drawer>
+            </div>
+        );
+    }
 }
+
+export default withStyles(styles)(AppShell);
