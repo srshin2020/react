79698967fa110ba369512df2c503a3189c6f0c5a
srshin2020 <sangrimshin2020@gmail.com>
2022-02-06 18:08:15 +0900

    firebase 배포

--- react-firebase-app/webpack.config.js
+++ react-firebase-app/webpack.config.js
@@ -1,5 +1,6 @@
 'use strict'
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -17,7 +18,14 @@ module.exports = {
         }]
     },
     devtool: 'eval-source-map',
-    plugins: [],
+    plugins: [
+        new CopyWebpackPlugin ({
+            patterns: [{
+            context: './public',
+            from: '*.*'
+            }]
+        })
+    ],
     devServer: {
         static: './public',
         host: 'localhost',
