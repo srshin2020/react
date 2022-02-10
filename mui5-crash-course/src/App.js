8ba123c5d114be34de97f99ed850edf15d97b1ed
srshin2020 <sangrimshin2020@gmail.com>
2022-02-10 20:56:07 +0900

    main page

--- mui5-crash-course/src/App.js
+++ mui5-crash-course/src/App.js
@@ -1,17 +1,38 @@
 import { Container, Grid } from '@mui/material';
 import './App.css';
 import Tourcard from './component/Tourcard';
+import Appbar from './component/Appbar';
+import cities from './data.json';
+import { Typography } from '@mui/material';
 
+// {
+//   "id": 1,
+//     "name": "Niagara Falls",
+//       "tours": [
+//       ]
+//}
 function App() {
   return (
     <div className="App">
-      <Container>
-        <Grid container spacing={5}>
-          <Tourcard />
-          <Tourcard />
-          <Tourcard />
-          <Tourcard />
-        </Grid>
+      <Appbar></Appbar>
+      <Container sx={{ marginY: 5 }}>
+        {cities.map((city) => {
+          return (
+            <div>
+              <Typography key={city.id} variant='h4' component='h2' marginTop={5} marginBottom={3} >
+                {city.name}
+              </Typography>
+              <Grid container spacing={5}>
+                {city.tours.map((tour, index) => {
+                  console.log(tour.name);
+                  return (
+                    <Tourcard tour={tour} key={index} />
+                  )
+                })}
+              </Grid>
+            </div>
+          )
+        })}
       </Container>
     </div>
   );
--- mui5-crash-course/src/component/Tourcard.js
+++ mui5-crash-course/src/component/Tourcard.js
@@ -13,34 +13,43 @@ const theme = createTheme({
         },
     },
 });
+//         {
+//           "id": 1,
+//           "name": "Immerse into the Falls",
+//           "duration": 5,
+//           "rating": 4.5,
+//           "numberOfReviews": 462,
+//           "price": 465,
+//           "image": "https://tcproduction.blob.core.windows.net/media/%7B240f8b72-1159-4fd3-a150-0a837f50ba4a%7D.2573758641_297d6d19fa_o.jpg"
+//         },
 
-const Tourcard = () => {
+const Tourcard = ({ tour }) => {
     return (
-        <Grid item xs={3}>
+        <Grid item sm={6} md={3}>
             <ThemeProvider theme={theme}>
-                <Paper elevation={5} >
+                <Paper elevation={5} className='paper'>
 
-                    <img src='https://images.unsplash.com/photo-1489447068241-b3490214e879?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+                    <img src={tour.image}
                         alt=''
                         className='img'
                     />
                     <Box paddingX={1}>
-                        <Typography component='h2' variant='subtitle1'>Immerse into the fall</Typography>
+                        <Typography component='h2' variant='subtitle1'>{tour.name}</Typography>
                         <Box sx={{
                             display: 'flex', alignItems: 'center'
                         }}>
                             <AccessTime sx={{ width: 12.5 }} />
-                            <Typography variant='body2' component='p' marginLeft={0.5}>5 hours</Typography>
+                            <Typography variant='body2' component='p' marginLeft={0.5}>{tour.duration} hours</Typography>
                         </Box>
                         <Box sx={{
                             display: 'flex', alignItems: 'center', marginTop: 3
                         }}>
-                            <Rating name='read-only' value={4.5} precision={0.5} size='small' />
-                            <Typography variant='body2' component='p' marginLeft={0.5}>4.5</Typography>
-                            <Typography variant='body2' component='p' marginLeft={1.5}>(655 Reviews)</Typography>
+                            <Rating name='read-only' value={tour.rating} precision={0.5} size='small' />
+                            <Typography variant='body2' component='p' marginLeft={0.5}>{tour.rating}</Typography>
+                            <Typography variant='body2' component='p' marginLeft={1.5}>({tour.numberOfReviews} Reviews)</Typography>
                         </Box>
                         <Box >
-                            <Typography variant='h6' component='h3' marginTop={0}>From C $140</Typography>
+                            <Typography variant='h6' component='h3' marginTop={0}>From C ${tour.price}</Typography>
                         </Box>
                     </Box>
                 </Paper>
