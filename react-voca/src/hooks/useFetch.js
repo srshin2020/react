89cb8d54173a26f977e5e34abbc3c5445105fde0
srshin2020 <sangrimshin2020@gmail.com>
2022-11-15 17:44:00 +0900

    modified:   react-voca/src/hooks/useFetch.js

--- react-voca/src/hooks/useFetch.js
+++ react-voca/src/hooks/useFetch.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-
+// url로부터 state에 해당하는 데이터를 가져와 데이터를 set한후 data를 return 함
 export default function useFetch(url) {
     const [data, setData] = useState([]);
 
