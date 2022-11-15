c391808ede1cee68121b75516a9bbc37cda048c4
srshin2020 <sangrimshin2020@gmail.com>
2022-11-15 17:00:52 +0900

    modified:   react-voca/src/component/Day.js modified:   react-voca/src/component/DayList.js

--- react-voca/src/component/Day.js
+++ react-voca/src/component/Day.js
@@ -1,11 +1,22 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import dummy from '../db/data.json';
+// import dummy from '../db/data.json';
 import Word from './Word';
 
 export default function Day() {
     const { day } = useParams();
-    console.log(day);
-    const wordList = dummy.words.filter((word) => String(word.day) === day);
+    const [wordList, setWords] = useState([]);
+    useEffect(() => {
+        console.log('useEffect');
+        const url = `http://localhost:3001/words?day=${day}`;
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => {
+                setWords(data);
+            });
+    }, [day]); // day가 업데이트되었을 때 호출되야 함.
+
+    // const wordList = words.filter((word) => String(word.day) === day);
     if (wordList.length === 0) {
         return <div>영어 단어장에 오신 것을 환영합니다! </div>;
     }
--- react-voca/src/component/DayList.js
+++ react-voca/src/component/DayList.js
@@ -1,10 +1,23 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import dummy from '../db/data.json';
+// import dummy from '../db/data.json';
 
 export default function DayList() {
+    const [days, setDays] = useState([]);
+
+    // 빈 배열 지정시 한번만 호출됨
+    useEffect(() => {
+        console.log('use effect 호출');
+        const url = 'http://localhost:3001/days';
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => {
+                setDays(data);
+            });
+    }, []);
     return (
         <ul className="list_day">
-            {dummy.days.map((day) => (
+            {days.map((day) => (
                 <li key={day.id}>
                     <Link to={`/day/${day.day}`}>Day {day.day}</Link>
                 </li>
